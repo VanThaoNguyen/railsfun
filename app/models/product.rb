0ab123c8749c58c14872d51337257670103e6acf
Thao <theo@futureworkz.com>
2015-07-07 12:02:50 +0700

    Add validate to category is not blank

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,17 @@
 class Product < ActiveRecord::Base
+
+	belongs_to :category
+
 	validate :title_is_shorter_than_description
 	validates :price, numericality: {greater_than: 0}
+	validates :category, presence: true
+
+	before_validation :strip_html_from_description
+	before_validation :title_is_downcase
 
 	scope :published,            ->{ where(published: true) }
 	scope :price_more_than,      ->( price ){ where('price > ?', price) }
-	scope :description_includes, -> (description){ where('description ILIKE ?', '%ruby%') } 
+	scope :description_includes, ->(description){ where('description ILIKE ?', '%ruby%') } 
 
 	def title_is_shorter_than_description
 		return if title.blank? or description.blank?
@@ -13,15 +20,12 @@ class Product < ActiveRecord::Base
 		end
 	end
 
-	before_save :strip_html_from_description
-	before_update :strip_html_from_description
-	before_validation :titile_is_downcase
-
 	def strip_html_from_description
 		self.description =  ActionView::Base.full_sanitizer.sanitize(self.description)
 	end
 
-	def titile_is_downcase
+	def title_is_downcase
 		self.title = self.title.downcase
 	end
+
 end
