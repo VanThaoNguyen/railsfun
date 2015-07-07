fa4bc68a5817d3b81045377b66958a4adf76cde4
Thao <theo@futureworkz.com>
2015-07-07 09:12:04 +0700

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+Product.create!(title: "Ruby book", description: "Learn Ruby progamming", price: 12.99, published: true)
+Product.create!(title: "Javascript book", description: "Learn Javascript progamming", price: 13.99, published: true)
+Product.create!(title: "SASS book", description: "Learn CSS with SASS", price: 19.99, published: true)
